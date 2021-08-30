7d8be9c18f494636dfcea5156aa7fb7c559fea3b
Friendlyuser1 <kitty.kat5532@gmail.com>
2021-08-30 17:53:22 +0100

    fin

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 var sharedData;
 var guessedLetters = [];
+var currentPart = 0;
+var badGuesses = 0;
+var goodGuesses = 0;
+var endOfGame = false;
 
 function generateGame() {
+	document.getElementById("word").style.fontSize = "70px";
+	guessedLetters = [];
+	currentPart = 0;
+	badGuesses = 0;
+	goodGuesses = 0;
+	endOfGame = false;
+	document.getElementById("guesses").innerHTML = ``;
+
 	var url = `https://random-word-api.herokuapp.com/word?number=1&swear=0`;
 	fetch(url)
 		.then(function (response) {
@@ -11,8 +23,6 @@ function generateGame() {
 			data = data.toString();
 			document.getElementById("word").innerHTML = data.replace(/./g, "_ ");
 			sharedData = btoa(data);
-			guessedLetters = [];
-			replaceword = [];
 		})
 		.catch(function (err) {
 			console.log(err);
@@ -22,17 +32,13 @@ function generateGame() {
 	clearCanvas();
 }
 
-function clearCanvas() {
-	var canvas = document.getElementById("stickcanvas");
-	var context = canvas.getContext("2d");
-	context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
 function buttonPressed(letter) {
+	if (endOfGame) return;
 	if (guessedLetters.includes(letter)) return;
 	var replaceword = [];
 	var openSplitData = atob(sharedData).split("");
 	var word = document.getElementById("word");
+	var isFalse;
 	replaceword = openSplitData.join(" ");
 	for (var i = 0; i < openSplitData.length; i++) {
 		if (openSplitData[i] != letter) {
@@ -41,6 +47,140 @@ function buttonPressed(letter) {
 			}
 		}
 	}
+	if (openSplitData.includes(letter) == false) {
+		isFalse = true;
+	}
+
 	word.innerHTML = replaceword;
+
+	if (isFalse) {
+		nextPart();
+		badGuesses++;
+	} else {
+		goodGuesses++;
+	}
+
+	word = document.getElementById("word");
+	if (word.innerHTML == atob(sharedData).split("").join(" ")) {
+		endGame();
+	}
 	guessedLetters.push(letter);
+	document.getElementById("guesses").innerHTML = `Guesses: ${
+		goodGuesses + badGuesses
+	} (Correct: ${goodGuesses}, Incorrect: ${badGuesses})`;
+}
+
+function clearCanvas() {
+	var canvas = document.getElementById("stickcanvas");
+	var context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw(part) {
+	var canvas = document.getElementById("stickcanvas");
+	var context = canvas.getContext("2d");
+	switch (part) {
+		case "gallows":
+			context.strokeStyle = "#000";
+			context.lineWidth = 8;
+			context.beginPath();
+			context.moveTo(175, 225);
+			context.lineTo(5, 225);
+			context.moveTo(40, 225);
+			context.lineTo(25, 5);
+			context.lineTo(100, 5);
+			context.lineTo(100, 25);
+			context.stroke();
+			break;
+
+		case "head":
+			context.lineWidth = 5;
+			context.beginPath();
+			context.arc(100, 50, 25, 0, Math.PI * 2, true);
+			context.closePath();
+			context.stroke();
+			break;
+
+		case "body":
+			context.beginPath();
+			context.moveTo(100, 75);
+			context.lineTo(100, 140);
+			context.stroke();
+			break;
+
+		case "rightHarm":
+			context.beginPath();
+			context.moveTo(100, 85);
+			context.lineTo(60, 100);
+			context.stroke();
+			break;
+
+		case "leftHarm":
+			context.beginPath();
+			context.moveTo(100, 85);
+			context.lineTo(140, 100);
+			context.stroke();
+			break;
+
+		case "rightLeg":
+			context.beginPath();
+			context.moveTo(100, 140);
+			context.lineTo(80, 190);
+			context.stroke();
+			break;
+
+		case "rightFoot":
+			context.beginPath();
+			context.moveTo(82, 190);
+			context.lineTo(70, 185);
+			context.stroke();
+			break;
+
+		case "leftLeg":
+			context.beginPath();
+			context.moveTo(100, 140);
+			context.lineTo(125, 190);
+			context.stroke();
+			break;
+
+		case "leftFoot":
+			context.beginPath();
+			context.moveTo(122, 190);
+			context.lineTo(135, 185);
+			context.stroke();
+			break;
+	}
+}
+
+function nextPart() {
+	var draws = [
+		"gallows",
+		"head",
+		"body",
+		"rightHarm",
+		"leftHarm",
+		"rightLeg",
+		"leftLeg",
+		"rightFoot",
+		"leftFoot",
+	];
+	draw(draws[currentPart]);
+	currentPart++;
+	if (currentPart >= 9) {
+		endGame();
+	}
+}
+
+function endGame() {
+	endOfGame = true;
+	var output = document.getElementById("word");
+	document.getElementById("word").style.fontSize = "25px";
+	if (currentPart >= 8) {
+		output.innerHTML =
+			"You ran out of guesses! Game over - Press New Word to play again";
+		return;
+	}
+	output.innerHTML = `Well done! The word was: ${atob(
+		sharedData
+	)}. Press New Word to play again`;
 }
